8e5b5d968aca6c8817870439074081a1b212cb88
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-06-19 19:36:49 +0000

    19.06

--- Projeto/main.js
+++ Projeto/main.js
@@ -1,4 +1,6 @@
 let bts = document.querySelectorAll("div.genius > *:not(.pontuacao)")
+let ordem = [ 0, 3, 1, 2]
+let velocidade = 1000
 
 function ligar (item) {
     bts[item].classList.add("on") /*Adiciona a classe '.on'*/
@@ -10,5 +12,17 @@ function desligar (item) {
 
 function piscar(item) {
     ligar(item)
-    setTimeout(function () { desligar(item) }, 1000) /*Depois de determinado intervalo de tempo, ele desliga*/
+    setTimeout(function () { desligar(item) }, velocidade) /*Depois de determinado intervalo de tempo, ele desliga*/
+}
+
+function apresentarSequencia () {
+    let index = 0 /*Posiçao atual do array*/
+    let interval = setInterval(function () { /*Inicia a leitura do intervalo*/
+        if (index >= ordem.length) { /*Compara a ordem da sequencia atual, com a sequencia ja dfinida*/
+            clearInterval(interval) /*Limpa a sequencia de leitura*/
+            return
+        }
+        let atual = ordem[index++] /*Pega o valor da posicao atual e soma +1*/
+        piscar(atual)
+    }, velocidade + 300)
 }
