04945c3a0ea0d1de3b3ed6eb4660d9b75e218c85
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-11-12 01:14:46 +0000

    ...

--- 001 - Menu/main.js
+++ 001 - Menu/main.js
@@ -1,17 +1,61 @@
-const btToggleMenu = document.querySelector(".menu-hamburger")
 
-btToggleMenu.addEventListener("click", () => {
-  document.body.classList.toggle("open-nav")
-})
+  const btToggleMenu = document.querySelector(".menu-hamburger");
+  const mainMenu = document.querySelector(".main-menu");
 
-const dialog = document.querySelector("dialog")
-const btDialog = document.querySelector(".bt-dialog")
-const btModal = document.querySelector(".bt-modal")
+  btToggleMenu.addEventListener("click", () => {
+      document.body.classList.toggle("open-nav");
+      toggleHamburgerVisibility();
+  });
 
-btDialog.addEventListener('click', () => dialog.show())
-btModal.addEventListener('click', () => dialog.showModal())
+  const dialogButtons = document.querySelectorAll(".bt-dialog");
+  const dialogs = document.querySelectorAll("dialog");
 
-document.querySelectorAll("dialog button").forEach(
-  bt => bt.addEventListener("click", () => dialog.close()
-  )
-)
+  dialogButtons.forEach((bt, index) => {
+      bt.addEventListener('click', () => {
+          dialogs[index].showModal();
+          closeMenu();
+      });
+  });
+
+  dialogs.forEach((dialog) => {
+      dialog.addEventListener("click", (event) => {
+          event.stopPropagation();
+      });
+
+      dialog.querySelectorAll("button").forEach((button) => {
+          button.addEventListener("click", () => {
+              dialog.close();
+          });
+      });
+  });
+
+  
+
+  mainMenu.addEventListener("click", (event) => {
+      event.stopPropagation();
+      if (!mainMenu.contains(event.target) && document.body.classList.contains("open-nav")) {
+        closeMenu();
+    }
+  });
+
+  function toggleHamburgerVisibility() {
+      btToggleMenu.classList.toggle("visible", document.body.classList.contains("open-nav"));
+  }
+
+  function closeMenu() {
+      document.body.classList.remove("open-nav");
+      toggleHamburgerVisibility();
+  }
+
+  const darkModeToggle = document.getElementById("darkModeToggle");
+  const body = document.body;
+  
+  darkModeToggle.addEventListener("change", () => {
+      if (darkModeToggle.checked) {
+          body.classList.add("dark-mode");
+      } else {
+          body.classList.remove("dark-mode");
+      }
+  });
+  
+
