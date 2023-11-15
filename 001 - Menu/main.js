e935dcbd351c56c1ef0ceed3c1e4b84ea1b32999
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-11-15 20:03:02 +0000

    Menu finaçizado

--- 001 - Menu/main.js
+++ 001 - Menu/main.js
@@ -1,61 +1,79 @@
 
-  const btToggleMenu = document.querySelector(".menu-hamburger");
-  const mainMenu = document.querySelector(".main-menu");
+const btToggleMenu = document.querySelector(".menu-hamburger");
+const mainMenu = document.querySelector(".main-menu");
+const dialogButtons = document.querySelectorAll(".bt-dialog");
+const dialogs = document.querySelectorAll("dialog");
+const darkModeToggle = document.getElementById("darkModeToggle");
+const body = document.body;
 
-  btToggleMenu.addEventListener("click", () => {
-      document.body.classList.toggle("open-nav");
-      toggleHamburgerVisibility();
-  });
+btToggleMenu.addEventListener("click", () => {
+    document.body.classList.toggle("open-nav");
+    toggleHamburgerVisibility();
+});
 
-  const dialogButtons = document.querySelectorAll(".bt-dialog");
-  const dialogs = document.querySelectorAll("dialog");
+dialogButtons.forEach((bt, index) => {
+    bt.addEventListener('click', () => {
+        dialogs[index].showModal();
+        closeMenu();
+    });
+});
 
-  dialogButtons.forEach((bt, index) => {
-      bt.addEventListener('click', () => {
-          dialogs[index].showModal();
-          closeMenu();
-      });
-  });
+dialogs.forEach((dialog) => {
+    dialog.addEventListener("click", (event) => {
+        event.stopPropagation();
+    });
 
-  dialogs.forEach((dialog) => {
-      dialog.addEventListener("click", (event) => {
-          event.stopPropagation();
-      });
+    dialog.querySelectorAll("button").forEach((button) => {
+        button.addEventListener("click", () => {
+            dialog.close();
+        });
+    });
+});
 
-      dialog.querySelectorAll("button").forEach((button) => {
-          button.addEventListener("click", () => {
-              dialog.close();
-          });
-      });
-  });
-
-  
-
-  mainMenu.addEventListener("click", (event) => {
-      event.stopPropagation();
-      if (!mainMenu.contains(event.target) && document.body.classList.contains("open-nav")) {
+mainMenu.addEventListener("click", (event) => {
+    event.stopPropagation();
+    if (!mainMenu.contains(event.target) && document.body.classList.contains("open-nav")) {
         closeMenu();
     }
-  });
+});
 
-  function toggleHamburgerVisibility() {
-      btToggleMenu.classList.toggle("visible", document.body.classList.contains("open-nav"));
-  }
+function toggleHamburgerVisibility() {
+    btToggleMenu.classList.toggle("visible", document.body.classList.contains("open-nav"));
+}
 
-  function closeMenu() {
-      document.body.classList.remove("open-nav");
-      toggleHamburgerVisibility();
-  }
+function closeMenu() {
+    document.body.classList.remove("open-nav");
+    toggleHamburgerVisibility();
+}
 
-  const darkModeToggle = document.getElementById("darkModeToggle");
-  const body = document.body;
-  
-  darkModeToggle.addEventListener("change", () => {
-      if (darkModeToggle.checked) {
-          body.classList.add("dark-mode");
-      } else {
-          body.classList.remove("dark-mode");
-      }
-  });
-  
+darkModeToggle.addEventListener("change", () => {
+    if (darkModeToggle.checked) {
+        body.classList.add("dark-mode");
+        return
+    }
+    body.classList.remove("dark-mode");
+});
+
+/* ============================================ */
+
+function toggleChat() {
+    const chatContainer = document.querySelector('.chat-container');
+    chatContainer.style.display = chatContainer.style.display === 'none' ? 'flex' : 'none';
+}
+
+function sendMessage() {
+    const input = document.querySelector('.chat-input input');
+    const message = input.value.trim();
+
+    if (message !== '') {
+        const chatMessages = document.querySelector('.chat-messages');
+        const newMessage = document.createElement('div');
+        newMessage.textContent = message;
+        chatMessages.appendChild(newMessage);
+        // Limpa o campo de entrada
+        input.value = '';
+    }
+}
+
+
 
